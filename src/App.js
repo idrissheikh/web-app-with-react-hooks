886d611b89db7820a82bef0d014690f3e1f7c89b
idrissheikh-idris <s310058@oslomet.no>
2020-04-21 01:56:12 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import "./App.css";
+
+function App() {
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users").then(response =>
+      response.json().then(users => setMonsters(users))
+    );
+  });
+
+  return (
+    <div className="App">
+      <CardList monsters={monsters}></CardList>
+    </div>
+  );
+}
+
+export default App;
